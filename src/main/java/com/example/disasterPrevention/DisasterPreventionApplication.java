a515eb9e2b834e9f83edbff138966654c1cc7e3a
mnssu <mns0904.pj@gmail.com>
2022-10-06 18:20:01 +0900

    first commit

---
+++ src/main/java/com/example/disasterPrevention/DisasterPreventionApplication.java
@@ -0,0 +1,13 @@
+package com.example.disasterPrevention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterPreventionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisasterPreventionApplication.class, args);
+	}
+
+}
